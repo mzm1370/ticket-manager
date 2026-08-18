407f42428f4e408229aeab6365f46c0abee26ab5
meysam zohouri <m.zohouri@apk-group.net>
2026-08-18 14:09:40 +0330

    fix: update tickets controller spec for repository DI

--- apps/api/src/tickets/tickets.controller.spec.ts
+++ apps/api/src/tickets/tickets.controller.spec.ts
@@ -1,6 +1,8 @@
 import { Test, TestingModule } from '@nestjs/testing';
+import { getRepositoryToken } from '@nestjs/typeorm';
 import { TicketsController } from './tickets.controller';
 import { TicketsService } from './tickets.service';
+import { Ticket } from './entities/ticket.entity';
 
 describe('TicketsController', () => {
   let controller: TicketsController;
@@ -8,7 +10,10 @@ describe('TicketsController', () => {
   beforeEach(async () => {
     const module: TestingModule = await Test.createTestingModule({
       controllers: [TicketsController],
-      providers: [TicketsService],
+      providers: [
+        TicketsService,
+        { provide: getRepositoryToken(Ticket), useValue: {} },
+      ],
     }).compile();
 
     controller = module.get<TicketsController>(TicketsController);
